ae472dc548dedc353f28377385426d4b98098ae5
Curtis Hill <curtis@cstudio.ca>
2023-10-19 15:10:48 -0700

    remove netlify form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,5 @@
-import React, { useState } from 'react';
 
 const Contact = ({ activeSection }) => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
 
   return (
     <section id="contact" className='section'>
@@ -21,41 +10,7 @@ const Contact = ({ activeSection }) => {
           </h2>
         </div>
         <div className='contact-form'>
-          <form name="contact-form" netlify>
-            <div className="form-group">
-              <label htmlFor="name">Name:</label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">Email:</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="message">Message:</label>
-              <textarea
-                id="message"
-                name="message"
-                value={formData.message}
-                onChange={handleChange}
-                required
-              ></textarea>
-            </div>
-            <button type="submit">Submit</button>
-          </form>
+          FORM
         </div>
       </div>
     </section>
