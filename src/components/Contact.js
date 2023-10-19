e84a745d66065550c25e7bd4209a1a8c26ec56b8
Curtis Hill <curtis@cstudio.ca>
2023-10-19 15:02:54 -0700

    remove handleSubmit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,27 +12,6 @@ const Contact = ({ activeSection }) => {
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch('/.netlify/functions/send-email', {
-        method: 'POST',
-        body: JSON.stringify(formData),
-      });
-  
-      if (response.ok) {
-        // Success: You can handle success actions here (e.g., show a success message).
-        console.log('Form submitted successfully');
-      } else {
-        // Error: You can handle error actions here (e.g., show an error message).
-        console.error('Form submission failed');
-      }
-    } catch (error) {
-      console.error('An error occurred:', error);
-    }
-  };  
-  
-
   return (
     <section id="contact" className='section'>
       <div className='section-content'>
@@ -42,7 +21,7 @@ const Contact = ({ activeSection }) => {
           </h2>
         </div>
         <div className='contact-form'>
-          <form name="contact-form" netlify onSubmit={handleSubmit}>
+          <form name="contact-form" netlify>
             <div className="form-group">
               <label htmlFor="name">Name:</label>
               <input
