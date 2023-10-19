4cf5dc7d61cdcf62b9cd978ad46105c24f4f2e89
Curtis Hill <curtis@cstudio.ca>
2023-10-19 13:43:20 -0700

    setup netlify for contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Contact = ({ activeSection }) => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('/.netlify/functions/send-email', {
+      method: 'POST',
+      body: JSON.stringify(formData),
+    });
+    
+    console.log(formData);
+  };
+
   return (
     <section id="contact" className='section'>
       <div className='section-content'>
@@ -10,11 +31,45 @@ const Contact = ({ activeSection }) => {
           </h2>
         </div>
         <div className='contact-form'>
-          FORM
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="name">Name:</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="message">Message:</label>
+              <textarea
+                id="message"
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                required
+              ></textarea>
+            </div>
+            <button type="submit">Submit</button>
+          </form>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
