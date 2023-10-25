fabb037911a964fdb98529936caaffad10f27eac
Curtis Hill <curtis@cstudio.ca>
2023-10-24 17:09:13 -0700

    add technologies tooltip

--- src/assets/IconInfo.js
+++ src/assets/IconInfo.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const IconInfo = () => {
+  const [strokeColor, setStrokeColor] = useState("#000000");
+
+  const handleMouseOver = () => {
+    setStrokeColor("#858585");
+  };
+
+  const handleMouseOut = () => {
+    setStrokeColor("#000");
+  };
+
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="48px"
-      height="48px"
+      width="24px"
+      height="24px"
       viewBox="0 0 64 64"
       strokeWidth="3"
-      stroke="#000000"
+      stroke={strokeColor}
       fill="none"
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
     >
       <line x1="32.06" y1="17.55" x2="32.06" y2="16.88" strokeLinecap="round" />
       <line x1="32.06" y1="45.3" x2="32.06" y2="24.87" strokeLinecap="round" />
@@ -19,3 +31,4 @@ const IconInfo = () => {
 };
 
 export default IconInfo;
+
--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
+import IconInfo from '../assets/IconInfo'
+import IconClose from '../assets/IconClose'
 import IconBadge from '../assets/IconBadge'
 import IconResume from '../assets/IconResume'
 import IconCreditCard from '../assets/IconCreditCard'
 import IconSupport from '../assets/IconSupport'
 
 const About = ({ activeSection }) => {
+  const [visibleTooltip, setVisibleTooltip] = useState(null);
+
+  const technologies = [
+    { name: 'HTML', tooltip: 'Hypertext Markup Language is the backbone of web pages, defining the structure and presentation of content on the Internet.' },
+    { name: 'CSS', tooltip: 'Cascading Style Sheets control the visual presentation and layout of HTML elements, enabling stylized, responsive designs.' },
+    { name: 'JavaScript', tooltip: 'JavaScript enables interactive and dynamic user experiences in the browser. It is a key component of modern web development.' },
+    { name: 'Shopify Liquid', tooltip: 'Shopify Liquid is an open-source template language specifically designed for customizing Shopify’s eCommerce themes.' },
+    { name: 'Shopify CLI', tooltip: 'The Shopify Command Line Interface is a tool that facilitates building and managing Shopify themes and apps, streamlining the development workflow.' },
+    { name: 'React', tooltip: 'React is a JavaScript library developed by Facebook for building user interfaces. It is renowned for its virtual DOM and component-based architecture.' },
+    { name: 'Node', tooltip: 'Node.js is a runtime environment that executes JavaScript on the server side, allowing the development of scalable network applications.' },
+    { name: 'MongoDB', tooltip: 'MongoDB is a NoSQL database that offers high performance and scalability. It uses a flexible, JSON-like document model for data storage.' },
+    { name: 'GitHub', tooltip: 'GitHub is a web-based platform that uses Git for version control, allowing multiple people to collaborate on software projects.' },
+    { name: 'Adobe Photoshop', tooltip: 'Adobe Photoshop is a leading graphic editing software, primarily used for raster graphics, image manipulation, and digital art.' },
+    { name: 'Adobe XD', tooltip: 'Adobe XD is a vector-based design tool used for creating user interfaces and prototypes for web and mobile apps.' },
+    { name: 'Figma', tooltip: 'Figma is a cloud-based design tool that allows real-time collaboration. It is used for UI and UX design, prototyping, and more.' },
+    { name: 'GPT-4', tooltip: 'The GPT-4 (Generative Pre-trained Transformer 4) model by OpenAI is one of the latest advancements in artificial intelligence, offering sophisticated natural language understanding and generation.' }
+  ];  
+
+  const handleTooltip = (index) => {
+    setVisibleTooltip(index === visibleTooltip ? null : index);
+  };
   return (
     <section id="about" className='section alt-bg'>
       <div className='section-content'>
@@ -15,22 +38,29 @@ const About = ({ activeSection }) => {
         </div>
         <div className='about-video-with-text-container'>
           <div className='about-text-section'>
-            <p className='about-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p className='about-text'>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             <p className='about-text'>Technologies I use:</p>
             <div className='about-technology-container'>
-              <p className='about-technology'>HTML</p>
-              <p className='about-technology'>CSS</p>
-              <p className='about-technology'>JavaScript</p>
-              <p className='about-technology'>Shopify Liquid</p>
-              <p className='about-technology'>Shopify CLI</p>
-              <p className='about-technology'>React</p>
-              <p className='about-technology'>Node</p>
-              <p className='about-technology'>MongoDB</p>
-              <p className='about-technology'>Github</p>
-              <p className='about-technology'>Adobe Photoshop</p>
-              <p className='about-technology'>Adobe XD</p>
-              <p className='about-technology'>Figma</p>
-              <p className='about-technology'>GPT-4</p>
+              {technologies.map((tech, index) => (
+                <div key={index} className='about-technologies'>
+                  <div className='about-technology'>
+                    <p>{tech.name}</p>
+                    <span className='tooltip-icon' onClick={() => handleTooltip(index)}>
+                      <IconInfo />
+                    </span>
+                  </div>
+                  {visibleTooltip === index && (
+                    <div className='tooltip-popup'>
+                      <p className='tooltip-content'>
+                        <div className='tooltip-close' onClick={() => handleTooltip(null)}>
+                          <IconClose />
+                        </div>
+                        {tech.tooltip}
+                      </p>
+                    </div>
+                  )}
+                </div>
+              ))}
             </div>
           </div>
           <div className='about-video-section'>
